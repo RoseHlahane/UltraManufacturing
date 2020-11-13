7203a3db23a46252d3fda81fc7091a4a733146db
LeratoHlahane <61046663+LeratoHlahane@users.noreply.github.com>
2020-11-13 03:03:23 +0200

    Commit eight

---
+++ UltraManufacturing/Models/AuthInfo.cs
@@ -0,0 +1,26 @@
+﻿using UltraManufacturing.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltraManufacturing.Models
+{
+    public class AuthInfo
+    {
+        public AuthInfo()
+        {
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<string> Permissions { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public bool IsAuthenticated => (UserId > 0);
+    }
+}
